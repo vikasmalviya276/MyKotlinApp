38aed992a7e424a97eab4192023e04a0f93315b1
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-24 23:45:14 +0530

    @author vikas malviya Date:24-july-2020 change: add home screen

---
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.hi.mykotlinapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setVisisbility();
+    }
+
+    private fun setVisisbility() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    }
+}
