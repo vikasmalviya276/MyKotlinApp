a6ae7eec8ee7f21d14e860f281d53d87c7c5f32b
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-25 20:10:36 +0530

    Merge branch 'master' of https://github.com/vikasmalviya276/MyKotlinApp into new_feature_rachit
    # Conflicts:
    #	app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt

--- app/src/main/java/com/example/hi/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val result = sum(5, 6)
         print(result)
-showNumber()
+        showNumber()
     }
 
     private fun showNumber() {
