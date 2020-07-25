b62c14bed12b7b7ef3ed2fde298f939e0f6adb96
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-25 15:14:48 +0530

    @author vikas malviya Date:24-july-2020 change: add home screen

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -8,46 +8,5 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        setVisisbility();
-        setDateOnScreen();
-        setMyDate();
-        setNewFeatureBranch();
-        setNewDate();
-        setRachitBranch();
-        setLocallyModofied();
-        setVikas();
-    }
-
-    private fun setVikas() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun setLocallyModofied() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun setRachitBranch() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun setNewDate() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun setNewFeatureBranch() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun setMyDate() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun setDateOnScreen() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun setVisisbility() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-
     }
 }
