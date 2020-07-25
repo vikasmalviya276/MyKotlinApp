d2ed3cb45399c147d3c487eeaa834b84a0d63ea8
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-25 15:39:35 +0530

    @author vikas malviya Date:24-july-2020 change: add home screen

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -8,6 +8,11 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        createBranch();
 
+    }
+
+    private fun createBranch() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
