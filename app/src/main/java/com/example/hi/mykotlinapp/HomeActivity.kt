bafc16433363a966e4ce37f6fd03e58687973052
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-25 17:04:08 +0530

    Merge branch 'master' of https://github.com/vikasmalviya276/MyKotlinApp into new_feature_rachit
    # Conflicts:
    #	app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -8,10 +8,10 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        showRachit()
+        showSanjay()
     }
 
-    private fun showRachit() {
+    private fun showSanjay() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
