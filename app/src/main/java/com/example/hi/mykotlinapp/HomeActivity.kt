07657f49206247df9ab405f2b57da7faa1a83f7a
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-25 16:10:28 +0530

    @author vikas malviya Date:25-july-2020 change: add home screen screen

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -9,9 +9,13 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         showRachit()
+showRachit2()
 
 
+    }
 
+    private fun showRachit2() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     private fun showRachit() {
