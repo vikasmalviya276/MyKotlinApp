065246fd28f55d7c31c3a307e42cea1774346c68
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-25 16:53:04 +0530

    @author vikas malviya Date:25-july-2020 change: add home screen screen

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -8,35 +8,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        showRachit()
-        showRachit2()
-        showRachit3()
-        showVikas()
-        showVikas5()
-        showVikas6()
-    }
 
-    private fun showVikas6() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun showVikas5() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun showVikas() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun showRachit3() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun showRachit2() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun showRachit() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
