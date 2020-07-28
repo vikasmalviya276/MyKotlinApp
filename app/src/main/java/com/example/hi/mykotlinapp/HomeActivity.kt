4789fb11a30f78aded76cf600f9104083578c738
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-28 13:06:08 +0530

    add File

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -11,6 +11,11 @@ class HomeActivity : AppCompatActivity() {
         showVikas()
         showVikas1()
         showVikas2()
+        showVikas3()
+    }
+
+    private fun showVikas3() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     private fun showVikas2() {
