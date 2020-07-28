896036f8d24ce5dbb6656b0f3796a4869f4d43f2
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-28 13:07:05 +0530

    @author vikas malviya Date 28-july-2020 add showvikas1 method

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -8,25 +8,5 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        showVikas()
-        showVikas1()
-        showVikas2()
-        showVikas3()
-    }
-
-    private fun showVikas3() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun showVikas2() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun showVikas1() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private fun showVikas() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
