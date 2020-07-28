164bcc7b4a5cd6afdfd63daa8174fea6fde9ae52
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-07-28 14:02:10 +0530

    @author vikas malviya Date 28-july-2020 yyyyyyyyyyy

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -20,6 +20,11 @@ class HomeActivity : AppCompatActivity() {
         showVikas10()
         showVikas11()
         showVikas12()
+        showVikas13()
+    }
+
+    private fun showVikas13() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     private fun showVikas12() {
