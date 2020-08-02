93fbe0ddc17cd34fa7d37613d23779776e843797
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-08-02 14:02:51 +0530

    @author vikas malviya Date 28-july-2020 aaaaaaaaa

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -8,5 +8,20 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        showMaster()
+        showVikas()
+        showMaster2()
+    }
+
+    private fun showMaster2() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun showVikas() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun showMaster() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
