aa38506a1d8ab64b2c07b2ff59b9dc6eeede6e2a
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-08-03 18:13:14 +0530

    asasa

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -9,5 +9,10 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        showMaster()
+    }
+
+    private fun showMaster() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
