6ac107ed156f3e19ac7a787331e9e3bd44b5d0ae
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-08-03 17:09:27 +0530

    Revert "@author vikas malviya Date 28-july-2020 zzzzzz"
    This reverts commit 6596e5d

--- app/src/main/java/com/example/hi/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/MainActivity.kt
@@ -5,17 +5,9 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val result = sum(5, 6)
-        print(result)
-
-    }
-
-
-    fun sum(number1: Int, number2: Int): Int {
-        val add = number1 + number2
-        return add
     }
 }
