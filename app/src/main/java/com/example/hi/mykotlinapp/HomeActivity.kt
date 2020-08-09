c09d9301f9c509122f279948f019ffe5ca1b5ff2
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-08-09 22:04:42 +0530

    Revert "@author vikas malviya Date 28-july-2020 zzzzzz"
    This reverts commit 6596e5d

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -9,10 +9,10 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        showVikas1()
+ showRachiit1()
     }
 
-    private fun showVikas1() {
+    private fun showRachiit1() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
