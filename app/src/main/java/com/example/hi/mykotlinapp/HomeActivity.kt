475dcfc3460974aa096cbac3094b587adf1c9042
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-08-13 22:18:16 +0530

    @author vikas malviya Date 28-july-2020 zzzzzz" showVikas 3

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -9,6 +9,5 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        /*rachit*/
     }
 }
