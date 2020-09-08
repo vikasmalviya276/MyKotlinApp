ee82eb12270e11ecfc8c6e2887607444f090eb15
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-09-08 12:03:22 +0530

    @author vikas malviya Date 28-july-2020 zzzzzz" showVikas 3

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -10,6 +10,11 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         shoeRachit()
+        showRachit1()
+    }
+
+    private fun showRachit1() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     private fun shoeRachit() {
