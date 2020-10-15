d0ab4352b603416ff0161b8ee5c168305ccc743c
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-10-15 17:04:47 +0530

    Merge branch 'master' of https://github.com/vikasmalviya276/MyKotlinApp into new_feature_rachit
    # Conflicts:
    #	app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt

--- app/src/main/java/com/example/hi/mykotlinapp/Main2Activity.java
+++ app/src/main/java/com/example/hi/mykotlinapp/Main2Activity.java
@@ -9,5 +9,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        setRachit();
+    }
+
+    private void setRachit() {
+
     }
 }
