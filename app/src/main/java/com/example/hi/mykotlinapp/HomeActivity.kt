a6c708ac4193aa57743d0a311dc70698c8a2f5fb
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-10-15 17:34:14 +0530

    Revert "Merge branch 'master' of https://github.com/vikasmalviya276/MyKotlinApp into new_feature_rachit"
    This reverts commit 038335a

--- app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/com/example/hi/mykotlinapp/HomeActivity.kt
@@ -9,6 +9,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        /*vikas code*/
+/*vikas code*/
     }
 }
